f03fad1b9fa3009e8c81e24f80d76262fbe081a8
NSCarter <30809794+NSCarter@users.noreply.github.com>
2018-10-24 17:32:54 +0100

    Added comments

--- Player.h
+++ Player.h
@@ -4,89 +4,90 @@
 #include <vector>
 using namespace std;
 
-#include "libsqlite.hpp"
+#include "libsqlite.hpp" //The libsqlite library
 #ifndef PLAYER_H
 #define PLAYER_H
 
 class John 
 {
   private:
-    int gold, mapSize;
+    int gold; //Define the variables for the player's balance, name and inventory 
     string farmName;
     vector<pair<string, int>>  inventory;
     
   public:
     John()
     {
-        gold = 500;
-        mapSize = 10;
+        gold = 500; //Set a default balance and inventory
         inventory = {{"Shovel",1}};
     }
     
-    void outputGold()
+    void outputGold() //Print player's current balance
     {
         cout << gold << " gold" << endl;
     }
     
-    int addGold(int amount)
+    int addGold(int amount) //Add gold to the balance and output the new balance
     {
       gold += amount;
       outputGold();
       return gold;
     }
     
-    int subtractGold(int amount)
+    int subtractGold(int amount) //Take gold from the balance and output the new balance
     {
         gold -= amount;
         outputGold();
         return gold;
     }
     
-    string setFarmName(string name)
+    string setFarmName(string name) //Update the farm name
     {
         farmName = name;
         return farmName;
     }
     
-    void addToInv(string item, int quantity)
+    void addToInv(string item, int quantity) //Add an item to the inventory
     {
         bool found = false;
         
-        for (int i = 0; i < inventory.size(); i++)
+        for (int i = 0; i < inventory.size(); i++) //Look at each item in the inventory
         {
-            if(inventory[i].first == item)
+            if(inventory[i].first == item) //Check if the item is already in the inventory
             {
-              inventory[i].second += quantity;
+              inventory[i].second += quantity; //If the item is in the inventory, update the quantity
               found = true;
             }
         }
         
-        if (found == false)
+        if (found == false) //If the item is not already in the inventory
         {
-            inventory.emplace_back(item, quantity);
+            inventory.emplace_back(item, quantity); //Add the new item to the inventory
         }
     }
     
-    void takeFromInv(string item, int quantity)
+    void takeFromInv(string item, int quantity) //Take an item from the inventory
     {
-        for (int i = 0; i < inventory.size(); i++)
+        quantity -= 1;
+        
+        for (int i = 0; i < inventory.size(); i++) //Look at each item in the inventory
         {
             if (inventory[i].first == item)
             {
-                if (inventory[i].second -= quantity > 0)
+                if (inventory[i].second -= quantity > 0) //Check if there will still be more than one of the item lest
                 {
-                    inventory[i].second -= quantity;
+                    inventory[i].second -= quantity; //If there is enough, update the quantity 
                 }
                 else
                 {
-                    inventory.erase(inventory.begin() + i);
+                    inventory.erase(inventory.begin() + i); //Otherwise, delelte the item from the list
                 }
                 break;
             }
         }
     }
     
-    void outputInv()
+    void outputInv() //Print the contents of the inventory
     {
         for (int i = 0; i < inventory.size(); i++)
         {
@@ -94,67 +95,58 @@ class John
         }
     }
     
-    bool checkInInv(string item)
+    bool checkInInv(string item) //Check if an item is in the inventory
     {
         for (int i = 0; i < inventory.size(); i++)
         {
             if (inventory[i].first == item)
             {
-                return true;
+                return true; //If the item is found, return true
             }
         }
-        return false;
+        return false; //If the item is not found, return false
     }
     
-    void playerStats()
-    {
-        cout << "Stats" << endl;
-    }
-    
-    void save()
+    void save() //Save the contents of the player's inventory to the database
     {    
       try
       {
           int count;
 
-          sqlite::sqlite db("FarmJohnDatabase.sqlite");
-          auto cur = db.get_statement();
+          sqlite::sqlite db("FarmJohnDatabase.sqlite"); //Connect to the database file
+          auto cur = db.get_statement(); //Create a cursor
 
-          for (int i = 0; i < inventory.size(); i++)
+          for (int i = 0; i < inventory.size(); i++) //Go through each item in the inventor
           {
-              //cout << "here" << endl;
-              
-              cur -> set_sql("SELECT count(*) "
+              cur -> set_sql("SELECT count(*) " 
                              "FROM UserInventory "
                              "WHERE PlayerName = ? "
-                             "AND ItemName = ?");
-              //cout << "here" << endl;
-              cur -> prepare();
-              cout << "here" << endl;
-              cur -> bind(1, farmName);
+                             "AND ItemName = ?"); //Query to check if the item is already in the database
+
+              cur -> prepare(); 
+              cur -> bind(1, farmName); //Assign the values to the query
               cur -> bind(2, inventory[i].first);
               cur -> step();
 
-              int count = cur -> get_int(0);
+              int count = cur -> get_int(0); //Found out if the item was found in the database
 
-              cur -> reset();
-              //cout << "here" << endl;
+              cur -> reset(); //Reset the cursor
 
-              if (count == 0)
+              if (count == 0) //If the item is not in the database
               {
-                  cur -> set_sql("INSERT INTO UserInventory Values (?, ?, ?);");
+                  cur -> set_sql("INSERT INTO UserInventory Values (?, ?, ?);"); //Add the item to the database
                   cur -> prepare();
                   cur -> bind(1, farmName);
                   cur -> bind(2, inventory[i].first);
                   cur -> bind(3, inventory[i].second);
                   cur -> step();
               }
-              else
+              else //If the item is already in the database
               {
                   cur -> set_sql("UPDATE UserInventory "
                                  "SET NumInInventory = ? "
                                  "WHERE PlayerName = ? "
-                                 "AND ItemName = ?");
+                                 "AND ItemName = ?"); //Update the quantity in the database
                   cur -> prepare();
                   cur -> bind(1, inventory[i].second);
                   cur -> bind(2, farmName);
@@ -165,9 +157,9 @@ class John
               cur -> reset();
           }
       }
-      catch(sqlite::exception e)
+      catch(sqlite::exception e) //Catch any exceptions that occur
       {
-          std::cerr << e.what() << std::endl;
+          std::cerr << e.what() << std::endl; //Output the error
       }
     }
 };
@@ -185,6 +177,10 @@ int main()
     t.outputInv();
     cout << endl;
     t.takeFromInv("Seeds", 2);
+    
+    //bool a = t.checkInInv("Carrots");
+    //cout << a << endl;
+    
     t.outputInv();
     
     return 0;   
